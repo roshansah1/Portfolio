d4522848a75705d7c191a6c437eaa71fa24b1a26
Roshan sah <134045348+rsys-roshan-sah@users.noreply.github.com>
2026-01-23 19:15:35 +0530

    Initial commit: Portfolio coming soon page with clock animation

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import clockLogo from './assets/clock.gif'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={clockLogo} className="logo react" alt="Clock logo" />
+      </div>
+      <h1>Coming Soon...🔥</h1>
+    </>
+  )
+}
+
+export default App
